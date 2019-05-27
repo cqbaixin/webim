b73e4a966e0a7b6152358e7bc7c706c0f0089860
baixin <18883787780@163.com>
2019-05-27 17:26:44 +0800

    js

--- socket/src/main.js
+++ socket/src/main.js
@@ -6,8 +6,9 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
